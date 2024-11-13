0a33fc7bd538197bdbaadf737b414ea87e54ce30
Ornan Manguele <117445148+omanguele@users.noreply.github.com>
2024-11-12 23:26:11 -0500

    Update total-products.ts

--- api/total-products.ts
+++ api/total-products.ts
@@ -2,7 +2,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
 // Remplacez par vos informations Shopify
-const apiKey = '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';  // Clé API privée de Shopify
+const apiKey = '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';  // Clé API privée de Shopify
 const apiPassword = '487ce62bbf1395f5a4b96ff9ab309896';  // Mot de passe de l'API privée
 const shopDomain = 'noel-a-lhopital.myshopify.com';  // Domaine de votre boutique
 
