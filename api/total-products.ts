868ae2048d6dd70b9d5b25b4af3a329ea7707350
Ornan Manguele <117445148+omanguele@users.noreply.github.com>
2024-11-12 23:53:31 -0500

    Update total-products.ts

--- api/total-products.ts
+++ api/total-products.ts
@@ -1,17 +1,11 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import Shopify from 'shopify-api-node';
 
 // Informations de connexion Shopify
 const apiKeyShopify = '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'; // Votre clé API privée
 const apiSecret = '487ce62bbf1395f5a4b96ff9ab309896'; // Votre mot de passe API privé
 const shopifyDomain = 'noel-a-lhopital.myshopify.com'; // Domaine de votre boutique
 
-// Créer une instance de Shopify avec vos informations de connexion
-const shopify = new Shopify({
-  shopName: shopifyDomain,
-  apiKey: apiKeyShopify,
-  password: apiSecret,
-});
+
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
