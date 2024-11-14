e76eaedc177aa92c44290325bb06834526ad8cfe
Ornan Manguele <117445148+omanguele@users.noreply.github.com>
2024-11-14 08:11:31 -0500

    Update total-products.ts

--- api/total-products.ts
+++ api/total-products.ts
@@ -6,10 +6,10 @@ const shopDomain = 'noel-a-lhopital.myshopify.com';  // Domaine de votre boutiqu
 const accessToken = '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';  // Token d'accès (X-Shopify-Access-Token)
 
 // URL de l'API Admin de Shopify pour GraphQL
-const graphqlUrl = `https://${shopDomain}/admin/api/2024-10/graphql.json`;
+const graphqlUrl = https://${shopDomain}/admin/api/2024-10/graphql.json;
 
 // Définir la requête GraphQL pour récupérer les commandes et les produits
-const query = `
+const query = 
   query {
     orders(first: 250, query: "financial_status:paid") {
       edges {
@@ -20,7 +20,6 @@ const query = `
               node {
                 title
                 quantity
-                productId
               }
             }
           }
@@ -28,7 +27,7 @@ const query = `
       }
     }
   }
-`;
+;
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -48,20 +47,14 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     if (response.ok) {
       let totalProducts = 0;
 
-      // Exclure les produits avec l'ID spécifique
-      const excludedProductId = '49184372293936';
-
       // Parcourir les commandes et additionner le nombre de produits dans chaque ligne de commande
       data.data.orders.edges.forEach((order: any) => {
         order.node.lineItems.edges.forEach((item: any) => {
-          // Vérifier si l'ID du produit est différent de celui que l'on souhaite exclure
-          if (item.node.productId !== excludedProductId) {
-            totalProducts += item.node.quantity;
-          }
+          totalProducts += item.node.quantity;
         });
       });
 
-      // Renvoyer le nombre total de produits commandés, excluant l'ID spécifique
+      // Renvoyer le nombre total de produits commandés
       res.status(200).json({ totalProducts });
     } else {
       // Gestion des erreurs si la requête échoue
